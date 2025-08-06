384a776e7918c17e6dcc630fb2df124bd3edb70e
Mireia <mireiagonzalezz03@gmail.com>
2025-08-06 18:54:47 +0200

    added two traps: saw and spike ball

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var start_position = $StartPosition
+@onready var player = $Player
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("quit"):
@@ -8,6 +9,12 @@ func _process(_delta: float) -> void:
 	elif Input.is_action_just_pressed("reset"):
 		get_tree().reload_current_scene()
 
-func _on_deathzone_body_entered(player: Node2D) -> void:
+func _on_deathzone_body_entered(body: Node2D) -> void:
+	reset_player()
+
+func _on_trap_touched_player() -> void:
+	reset_player()
+
+func reset_player():
 	player.velocity = Vector2.ZERO
 	player.global_position = start_position.global_position
