087346169a8999bcc8d116eec5ab07480bc9db2b
Mireia <mireiagonzalezz03@gmail.com>
2025-08-07 23:37:43 +0200

    created level timer and reset player if they don't reach exit on time

--- scripts/level.gd
+++ scripts/level.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
 @export var next_level: PackedScene = null
+@export var level_time: int = 5
 
 @onready var start: StartSpawn = $Start
 @onready var exit = $Exit
 @onready var death_zone = $Deathzone
 
 var player: Player = null
+var timer_node: Timer = null
+var time_left
+var win: bool = false
 
 func _ready() -> void:
 	player = get_tree().get_first_node_in_group("player")
@@ -19,6 +23,22 @@ func _ready() -> void:
 	
 	exit.body_entered.connect(_on_exit_body_entered)
 	death_zone.body_entered.connect(_on_deathzone_body_entered)
+	
+	time_left = level_time
+	
+	timer_node = Timer.new()
+	timer_node.name = "Level Timer"
+	timer_node.wait_time = 1
+	timer_node.timeout.connect(_on_level_timer_timeout)
+	add_child(timer_node)
+	timer_node.start()
+
+func _on_level_timer_timeout() -> void:
+	if !win:
+		time_left -= 1
+		if time_left < 0:
+			reset_player()
+			time_left = level_time
 
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("quit"):
@@ -41,5 +61,6 @@ func _on_exit_body_entered(body: Node2D):
 		if next_level != null:
 			exit.animate()
 			player.active = false
+			win = true
 			await  get_tree().create_timer(1.5).timeout
 			get_tree().change_scene_to_packed(next_level)
